9036aed3d990b8896364bd6ce2be86e3ad260f17
Heitung Lau <heitung221@gmail.com>
2018-01-10 15:22:48 +0800

    small change

--- gdpiano.js
+++ gdpiano.js
@@ -342,7 +342,7 @@ $(document).ready(function(){
 		$(this).blur();
 		//Description
 		if (Layout == 1 || Layout == 2 || Layout == 3){
-			$("#tutorial").text("Instruction: Press A to play Do, press S to play Re, press D to play Mi, and etc. Press Shift + A to play the key between Do and Re, press Shift + S to play the key between Re and Mi, and etc. You can always play middle C by pressing LCtrl.");
+			$("#tutorial").text("Instruction: Press A to play Do, press S to play Re, press D to play Mi, and etc. Press Shift + A to play the key between Do and Re, press Shift + S to play the key between Re and Mi, and etc.");
 			}
 		else if (Layout == 4){
 			$("#tutorial").text("Instruction: A is Do, S is Re, D is Mi, F is Fa, G is So, and etc. W is the key between Do and Re (Di), E is the key between Re and Mi (Ri), and etc.");
@@ -386,8 +386,6 @@ $(document).ready(function(){
 				function play(inputNumber){
 					
 					var au = Dol + inputNumber;
-					
-					//var note = ["C2","C#2","D2","D#2","E2","F2","F#2","G2","G#2","A2","A#2","B2","C3","C#3","D3","D#3","E3","F3","F#3","G3","G#3","A3","A#3","B3","C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4","C5","C#5","D5","D#5","E5","F5","F#5","G5","G#5","A5","A#5","B5","C6","C#6","D6","D#6","E6","F6","F#6","G6","G#6","A6","A#6","B6","C7","C#7","D7","D#7","E7","F7","F#7","G7","G#7","A7","A#7","B7","C8"];
 					
 					if (chordToPlay < 37){
 					sources.triggerAttack(note[au]);
@@ -516,8 +514,6 @@ $(document).ready(function(){
 				
 						var au = Dol + inputNumber;
 					
-						//var note = ["C2","C#2","D2","D#2","E2","F2","F#2","G2","G#2","A2","A#2","B2","C3","C#3","D3","D#3","E3","F3","F#3","G3","G#3","A3","A#3","B3","C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4","C5","C#5","D5","D#5","E5","F5","F#5","G5","G#5","A5","A#5","B5","C6","C#6","D6","D#6","E6","F6","F#6","G6","G#6","A6","A#6","B6","C7","C#7","D7","D#7","E7","F7","F#7","G7","G#7","A7","A#7","B7","C8"];
-						
 						sources.triggerRelease(note[au]);	
 					}
 				//End of Stop					
